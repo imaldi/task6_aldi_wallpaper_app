bff459ba33c4db46125f4be6fa1722ac49831c5e
im_aldi <aldiirsanmajid@gmail.com>
2020-12-10 20:54:45 +0700

    coba set wallpaper, belum bisa

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,49 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
-import 'package:wallpaper_manager/wallpaper_manager.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+// import 'package:transparent_image/transparent_image.dart';
+// import 'package:wallpaper_manager/wallpaper_manager.dart';
+// import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'dart:async';
 import 'package:flutter/services.dart';
-import 'package:toast/toast.dart';
+// import 'package:toast/toast.dart';
+import 'package:wallpaperplugin/wallpaperplugin.dart';
 
 class Detail extends StatefulWidget {
-  final List<String> imagelist;
-  final int index;
-  Detail(this.imagelist, this.index);
-
+  // final List<String> imagelist;
+  // final int index;
+  final String imagePath;
+  // Detail(this.imagelist, this.index);
+  Detail(this.imagePath);
   @override
-  _Detail createState() => _Detail(this.imagelist, this.index);
+  _Detail createState() => _Detail(this.imagePath);
 }
 
 class _Detail extends State<Detail> {
-  final List<String> imagelist;
-  final int index;
-  _Detail(this.imagelist, this.index);
+  // final List<String> imagelist;
+  // final int index;
+  final String imagePath;
+  // _Detail(this.imagelist, this.index);
+  _Detail(this.imagePath);
   // ignore: unused_field
-  String _wallpaperFile = 'Unknown';
+  String _wallpaperStatus = "Initial";
 
-  Future<void> setWallpaperFromFile() async {
-    setState(() {
-      _wallpaperFile = "Loading";
-    });
-    String result;
-    var file = await DefaultCacheManager().getSingleFile(imagelist[index]);
+  // Future<void> setWallpaperFromFile() async {
+  //   setState(() {
+  //     _wallpaperFile = "Loading";
+  //   });
+  //   String result;
+  //   var file = await DefaultCacheManager().getSingleFile(imagePath);
+  //   // Platform messages may fail, so we use a try/catch PlatformException.
+  //   try {
+  //     result = await WallpaperManager.setWallpaperFromFile(
+  //         file.path,
+  //         // imagePath,
+  //         WallpaperManager.HOME_SCREEN);
+  //     Toast.show("Success", context, duration: Toast.LENGTH_LONG, gravity:  Toast.BOTTOM);
+  //   } on PlatformException {
+  //     result = 'Failed to get wallpaper.';
+  //   }
+  //
+  //   // If the widget was removed from the tree while the asynchronous platform
+  //   // message was in flight, we want to discard the reply rather than calling
+  //   // setState to update our non-existent appearance.
+  //   if (!mounted) return;
+  //
+  //   setState(() {
+  //     _wallpaperFile = result;
+  //   });
+  // }
+
+  Future<void> setWallpaperFromFile(String imagePath) async {
+    String wallpaperStatus = "Unexpected Result";
+    String _localFile = imagePath;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      result = await WallpaperManager.setWallpaperFromFile(
-          file.path, WallpaperManager.HOME_SCREEN);
-      Toast.show("Success", context, duration: Toast.LENGTH_LONG, gravity:  Toast.BOTTOM);
+      Wallpaperplugin.setWallpaperWithCrop(localFile: _localFile);
+      //uncomment below line to set wallpaper without cropping
+      //Wallpaperplugin.setAutoWallpaper(localFile: _localFile);
+      wallpaperStatus = "new Wallpaper set";
     } on PlatformException {
-      result = 'Failed to get wallpaper.';
+      print("Platform exception");
+      wallpaperStatus = "Platform Error Occured";
     }
-
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
     if (!mounted) return;
-
     setState(() {
-      _wallpaperFile = result;
+      _wallpaperStatus = wallpaperStatus;
     });
   }
 
@@ -58,11 +86,18 @@ class _Detail extends State<Detail> {
               margin: EdgeInsets.all(10),
               child: Column(
                 children: <Widget>[
-                  FadeInImage.memoryNetwork(placeholder:  kTransparentImage, image: this.imagelist[index]),
+                  // FadeInImage.memoryNetwork(placeholder:  kTransparentImage, image: this.imagePath),
+                  Image.asset(this.imagePath,height: 300,width: 300, fit: BoxFit.contain),
+                  Padding(padding: EdgeInsets.all(5),),
                   RaisedButton(
-                      child: Text('Set As Walpaper'),
+                    color: Colors.green,
+                      child: Text('Set As Walpaper',
+                          style: TextStyle(
+                            color: Colors.white,
+                          )
+                      ),
                       onPressed: (){
-                        setWallpaperFromFile();
+                        setWallpaperFromFile(imagePath);
                       }
                   )
                 ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:transparent_image/transparent_image.dart';
 import 'detail.dart';
 // import 'package:task_6_flutter_wallpaper_app/detail_pantai.dart';
 
@@ -60,17 +58,13 @@ class _MyHomePageState extends State<MyHomePage> {
   view(){
     if(home == 'sport'){
       return Sport();
-    }
-    if (home == 'nature') {
+    } else if (home == 'nature') {
       return Nature();
-    }
-    if (home == 'technology') {
+    } else if (home == 'technology') {
       return Technology();
-    }
-    if (home == 'art') {
+    }else if (home == 'art') {
       return Art();
-    }
-    if (home == 'place') {
+    } else if (home == 'place') {
       return Place();
     }
   }
@@ -96,6 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               home= 'sport';
                             });
                           },
+                          color: Colors.lightBlue,
                           child: Text('Sport')
                       ),
                       FlatButton(
@@ -104,6 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               home= 'nature';
                             });
                           },
+                          color: Colors.lightBlue,
                           child: Text('Nature')
                       ),
                       FlatButton(
@@ -112,6 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               home= 'technology';
                             });
                           },
+                          color: Colors.lightBlue,
                           child: Text('Technology')
                       ),
                       FlatButton(
@@ -120,6 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               home= 'art';
                             });
                           },
+                          color: Colors.lightBlue,
                           child: Text('Art')
                       ),
                       FlatButton(
@@ -128,6 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               home= 'place';
                             });
                           },
+                          color: Colors.lightBlue,
                           child: Text('Place')
                       ),
                     ],
@@ -151,45 +150,64 @@ class Sport extends StatefulWidget {
 
 class _Sport extends State<Sport> {
 
+  List<Container> sportContainerList = new List();
   List<String> sportlist= [
-    'https://i.pinimg.com/236x/07/f9/ab/07f9abf3002425a530db0af32b128134.jpg',
-    'https://i.pinimg.com/236x/a0/05/47/a0054732fdb1dd95b2283ee8c7fb5241.jpg',
-    'https://i.pinimg.com/236x/db/ef/df/dbefdf627b50f3040d55f394bd9de1d0.jpg',
-    'https://i.pinimg.com/236x/54/db/8b/54db8b2621ab2d17ece4dee81c623329.jpg',
-    'https://i.pinimg.com/236x/04/53/73/045373c9164756a785c56b00d5a0553a.jpg',
-    'https://i.pinimg.com/236x/e7/2e/d3/e72ed32d88257bf415696bd711582964.jpg',
-    'https://i.pinimg.com/236x/82/34/38/823438930d25d756df410ae8323a7fc7.jpg',
-    'https://i.pinimg.com/236x/2f/cb/61/2fcb614e41365751914933d9bd2f861f.jpg',
-    'https://i.pinimg.com/236x/98/da/e8/98dae897233f4621e36fcc875948652c.jpg',
-    'https://i.pinimg.com/236x/36/62/4e/36624efba3d7e2884cb0e4ba6595d0db.jpg',
-    'https://i.pinimg.com/236x/97/e4/b9/97e4b9390f30a86ea4afe540dec0d5f4.jpg',
-    'https://i.pinimg.com/236x/14/dc/f3/14dcf3a7c87625397f0de6a42b198b50.jpg',
-    'https://i.pinimg.com/236x/40/55/4c/40554c241e12c18d5dba55802f218b76.jpg',
+    's1.jpg',
+    's2.jpg',
+    's3.jpg',
+    's4.jpg',
+    's5.jpg',
+    's6.jpg',
+    's7.jpg',
+    's8.jpg',
+    's9.jpg',
+    's10.jpg',
+    's11.jpg',
+    's12.jpg',
+    's13.jpg',
   ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.all(10),
-        child:new StaggeredGridView.countBuilder(
-          shrinkWrap: true,
-          crossAxisCount: 4,
-          itemCount: sportlist.length,
-          itemBuilder: (BuildContext context, int index) => new Container(
-              child: InkWell(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> Detail(
-                      sportlist, index
-                  )));
-                },
-                child: FadeInImage.memoryNetwork(placeholder: kTransparentImage, image: sportlist[index], fit: BoxFit.cover,),
-              )
+    return GridView.count(crossAxisCount: 1, children: sportContainerList);
+  }
+  _buatContainerList() async {
+    for(var i = 0; i<sportlist.length; i++){
+      // final dataPantai = itemPantai[i];
+
+      final String image = sportlist[i];
+      sportContainerList.add(new Container(
+        padding: EdgeInsets.all(10),
+        child: Card(
+          child: InkWell(
+            onTap : (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> Detail(
+                  "gambar/sport/"+
+                      sportlist[i]
+              )));
+            },
+            child: Column(
+              children: [
+                Hero(
+                  tag: sportlist[i],
+                  child: Image.asset('gambar/sport/$image',height: 300,width: 300, fit: BoxFit.contain,),
+                ),
+                // Padding(
+                //   padding: EdgeInsets.all(1),
+                // ),
+                // Text(dataPantai['nama'],style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.lightBlue),)
+              ],
+            ),
           ),
-          staggeredTileBuilder: (int index) =>
-          new StaggeredTile.count(2, index.isEven ? 2 : 1),
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
-        )
-    );
+        ),
+      ));
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _buatContainerList();
   }
 }
 
@@ -203,13 +221,20 @@ class _Nature extends State<Nature> {
 
   List<Container> natureContainerList = new List();
   List<String> naturelist= [
-    'gili-trawangan-lombok-ntb.jpg',
-    'karimun-jawa-jawa-tengah.jpg',
-    'pantai-nembrala.jpg',
-    'pantai-nihiwatu-sumba-ntt.jpg',
-    'pantai-ora-maluku-tengah.jpg',
-    'pantai-padang-padang-bali.jpg',
-    'pantai-pasir-putih-karang-asem-bali.jpg',
+    'n1.jpg',
+    'n2.jpg',
+    'n3.jpg',
+    'n4.jpg',
+    'n5.jpg',
+    'n6.jpg',
+    'n7.jpg',
+    'n8.jpg',
+    'n9.jpg',
+    'n10.jpg',
+    'n11.jpg',
+    'n12.jpg',
+    'n13.jpg',
+    'n14.jpg',
   ];
 
   _buatContainerList() async {
@@ -222,12 +247,10 @@ class _Nature extends State<Nature> {
         child: Card(
           child: InkWell(
             onTap : (){
-              // Navigator.push(context, MaterialPageRoute(builder: (context) => PageDetailPantai(
-              //   nama: dataPantai['nama'],
-              //   gambar: gambarPantai,
-              //   provinsi: dataPantai['provinsi'],
-              //   deskripsi: dataPantai['deskripsi'],
-              // )));
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> Detail(
+                  "gambar/nature/"+
+                      naturelist[i]
+              )));
             },
             child: Column(
               children: [
@@ -270,45 +293,60 @@ class Technology extends StatefulWidget {
 class _Technology extends State<Technology> {
 
   List<String> technologylist= [
-    'https://cdn.pixabay.com/photo/2015/04/19/08/32/rose-729509__340.jpg',
-    'https://cdn.pixabay.com/photo/2018/10/30/16/06/water-lily-3784022__340.jpg',
-    'https://cdn.pixabay.com/photo/2015/04/19/08/33/technology-729512__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/02/27/06/43/cherry-tree-1225186__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/06/17/16/42/bouquet-1463562__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/09/22/15/42/rose-1687547__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/08/03/14/24/roses-1566792__340.jpg',
-    'https://cdn.pixabay.com/photo/2018/07/20/13/52/suntechnology-3550693__340.jpg',
-    'https://cdn.pixabay.com/photo/2015/06/08/15/00/daisy-801813__340.jpg',
-    'https://cdn.pixabay.com/photo/2017/10/15/15/58/spring-2854205__340.jpg',
-    'https://cdn.pixabay.com/photo/2018/08/10/21/53/dandelion-3597681__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/11/19/15/46/bloom-1839982__340.jpg',
-    'https://cdn.pixabay.com/photo/2017/10/10/14/54/plant-2837468__340.jpg',
-    'https://cdn.pixabay.com/photo/2019/03/24/14/45/crocus-4077848__340.jpg',
-    'https://cdn.pixabay.com/photo/2018/08/08/20/41/rose-3593112__340.jpg',
+    't1.jpg',
+    't2.jpg',
+    't3.jpg',
+    't4.jpg',
+    't5.jpg',
+    't6.jpg',
+    't7.jpg',
+    't8.jpg',
+    't9.jpg',
+    't10.jpg',
   ];
+
+  List<Container> technologyContainerList = new List();
+
+  _buatContainerList() async {
+    for(var i = 0; i<technologylist.length; i++){
+      // final dataPantai = itemPantai[i];
+
+      final String image = technologylist[i];
+      technologyContainerList.add(new Container(
+        padding: EdgeInsets.all(10),
+        child: Card(
+          child: InkWell(
+            onTap : (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> Detail(
+                  "gambar/technology/"+
+                      technologylist[i]
+              )));
+            },
+            child: Column(
+              children: [
+                Hero(
+                  tag: technologylist[i],
+                  child: Image.asset('gambar/technology/$image',height: 300,width: 300, fit: BoxFit.contain,),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ));
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _buatContainerList();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
         margin: EdgeInsets.all(10),
-        child: new StaggeredGridView.countBuilder(
-          shrinkWrap: true,
-          crossAxisCount: 4,
-          itemCount: technologylist.length,
-          itemBuilder: (BuildContext context, int index) => new Container(
-            child: InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> Detail(
-                    technologylist, index
-                )));
-              },
-              child: FadeInImage.memoryNetwork(placeholder: kTransparentImage, image: technologylist[index], fit: BoxFit.cover,),
-            ),
-          ),
-          staggeredTileBuilder: (int index) =>
-          new StaggeredTile.count(2, index.isEven ? 2 : 1),
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
-        )
+        child: GridView.count(crossAxisCount: 1, children: technologyContainerList)
     );
   }
 }
@@ -321,43 +359,61 @@ class Art extends StatefulWidget {
 class _Art extends State<Art> {
 
   List<String> artlist= [
-    'https://cdn.pixabay.com/photo/2018/08/21/23/29/fog-3622519__340.jpg',
-    'https://cdn.pixabay.com/photo/2015/12/01/20/28/fall-1072821__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/07/22/16/29/fog-1535201__340.jpg',
-    'https://cdn.pixabay.com/photo/2018/11/17/22/15/tree-3822149__340.jpg',
-    'https://cdn.pixabay.com/photo/2019/03/31/21/43/the-dark-hedges-4094148__340.jpg',
-    'https://cdn.pixabay.com/photo/2018/11/09/08/08/autumn-3804001__340.jpg',
-    'https://cdn.pixabay.com/photo/2015/12/08/00/39/foggy-1081915__340.jpg',
-    'https://cdn.pixabay.com/photo/2018/05/11/23/33/art-3392077__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/11/25/15/14/landscape-1858602__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/03/09/09/42/art-1245951__340.jpg',
-    'https://cdn.pixabay.com/photo/2016/03/27/19/49/nature-1283976__340.jpg',
-    'https://cdn.pixabay.com/photo/2017/02/12/17/35/sunbeam-2060662__340.jpg',
-    'https://cdn.pixabay.com/photo/2019/02/27/02/07/art-4023269__340.jpg',
+    'a1.jpg',
+    'a2.jpg',
+    'a3.jpg',
+    'a4.jpg',
+    'a5.jpg',
+    'a6.jpg',
+    'a7.jpg',
+    'a8.jpg',
+    'a9.jpg',
+    'a10.jpg',
+    'a11.jpg',
   ];
+
+  List<Container> artContainerList = new List();
+
+  _buatContainerList() async {
+    for(var i = 0; i<artlist.length; i++){
+      // final dataPantai = itemPantai[i];
+
+      final String image = artlist[i];
+      artContainerList.add(new Container(
+        padding: EdgeInsets.all(10),
+        child: Card(
+          child: InkWell(
+            onTap : (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> Detail(
+                  "gambar/art/"+
+                      artlist[i]
+              )));
+            },
+            child: Column(
+              children: [
+                Hero(
+                  tag: artlist[i],
+                  child: Image.asset('gambar/art/$image',height: 300,width: 300, fit: BoxFit.contain,),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ));
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _buatContainerList();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
         margin: EdgeInsets.all(10),
-        child: new StaggeredGridView.countBuilder(
-          shrinkWrap: true,
-          crossAxisCount: 4,
-          itemCount: artlist.length,
-          itemBuilder: (BuildContext context, int index) => new Container(
-            child: InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> Detail(
-                    artlist, index
-                )));
-              },
-              child: FadeInImage.memoryNetwork(placeholder: kTransparentImage, image: artlist[index], fit: BoxFit.cover,),
-            ),
-          ),
-          staggeredTileBuilder: (int index) =>
-          new StaggeredTile.count(2, index.isEven ? 2 : 1),
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
-        )
+        child: GridView.count(crossAxisCount: 1, children: artContainerList)
     );
   }
 }
@@ -370,44 +426,62 @@ class Place extends StatefulWidget {
 class _Place extends State<Place> {
 
   List<String> placelist= [
-    'https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/1624496/pexels-photo-1624496.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/3617500/pexels-photo-3617500.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/1266810/pexels-photo-1266810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/1562/italian-landscape-places-nature.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/1670187/pexels-photo-1670187.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/2437297/pexels-photo-2437297.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/1482927/pexels-photo-1482927.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/1366909/pexels-photo-1366909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/2437286/pexels-photo-2437286.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'https://images.pexels.com/photos/3551762/pexels-photo-3551762.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'p1.jpg',
+    'p2.jpg',
+    'p3.jpg',
+    'p4.jpg',
+    'p5.jpg',
+    'p6.jpg',
+    'p7.jpg',
+    'p8.jpg',
+    'p9.jpg',
+    'p10.jpg',
+    'p11.jpg',
+    'p12.jpg',
   ];
+
+  List<Container> placeContainerList = new List();
+
+  _buatContainerList() async {
+    for(var i = 0; i<placelist.length; i++){
+      // final dataPantai = itemPantai[i];
+
+      final String image = placelist[i];
+      placeContainerList.add(new Container(
+        padding: EdgeInsets.all(10),
+        child: Card(
+          child: InkWell(
+            onTap : (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> Detail(
+                  "gambar/place/"+
+                      placelist[i]
+              )));
+            },
+            child: Column(
+              children: [
+                Hero(
+                  tag: placelist[i],
+                  child: Image.asset('gambar/place/$image',height: 300,width: 300, fit: BoxFit.contain,),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ));
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _buatContainerList();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
         margin: EdgeInsets.all(10),
-        child: new StaggeredGridView.countBuilder(
-          shrinkWrap: true,
-          crossAxisCount: 4,
-          itemCount: placelist.length,
-          itemBuilder: (BuildContext context, int index) => new Container(
-            child: InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> Detail(
-                    placelist, index
-                )));
-              },
-              child: FadeInImage.memoryNetwork(placeholder: kTransparentImage, image: placelist[index], fit: BoxFit.cover,),
-            ),
-          ),
-          staggeredTileBuilder: (int index) =>
-          new StaggeredTile.count(2, index.isEven ? 2 : 1),
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
-        )
+        child: GridView.count(crossAxisCount: 1, children: placeContainerList)
     );
   }
 }
